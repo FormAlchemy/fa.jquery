bf19f779a9cf41d42991333c0969b26e4315faa3
Gael Pasgrimaud <gael@gawel.org>
2009-09-18 23:51:28 +0200

    fix compression

--- compress.sh
+++ compress.sh
@@ -3,4 +3,4 @@
 D="fa/jquery/jquery-ui-1.7.2.custom";
 CMD="python compressor.py  -c $HOME/jar/yuicompressor-2.4.2.jar "
 $CMD -o $D/fa.jquery.min.css -t css $(find $D  -mindepth 2 -maxdepth 2 -name "*.css")
-$CMD -o $D/fa.jquery.min.js $(find $D/js -name "*.js") $(find $D/co* -name "*.js")
+$CMD -o $D/fa.jquery.min.js $(find $D/js -name "jquery-*.js") $(find $D/js -name "jquery.*.js") $(find $D/co* -name "*.js")
