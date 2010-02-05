f02cc3ec9a645bad67b877a3a6a2c84e01ee9c77
Gael Pasgrimaud <gael@gawel.org>
2010-02-05 14:03:36 +0100

    add markup preview

--- fa/jquery/wsgi.py
+++ fa/jquery/wsgi.py
@@ -10,9 +10,35 @@ dirname = os.path.join(os.path.dirname(__file__), jquery_ui)
 def html_headers():
     return ''
 
+class Static(StaticURLParser):
+    """subclass to add a few helpers like markup preview"""
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO']
+        if path == '/markup_preview.html':
+            # markup preview helper
+            req = Request(environ)
+            resp = Response()
+            markup = req.POST.get('markup', 'textile')
+            value = req.POST['value']
+            try:
+                if markup == 'textile':
+                    from textile import textile
+                    value = textile(value)
+                elif markup == 'bbcode':
+                    from postmarkup import render_bbcode
+                    value = render_bbcode(value)
+            except:
+                pass
+            if isinstance(value, unicode):
+                value = value.encode('utf-8')
+            resp.body = value
+            return resp(environ, start_response)
+        return StaticURLParser.__call__(self, environ, start_response)
+
 def StaticApp(*args, **local_conf):
     path = local_conf.get('jquery_ui', dirname)
-    return StaticURLParser(path)
+    return Static(path)
 
 doc = """paste entry point. Generate a StaticURLParser to serve js stuff.
 
