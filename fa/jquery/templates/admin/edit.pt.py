d243f71babe617f0f3b8bf9e3d4a86eff11997f3
Gael Pasgrimaud <gael@gawel.org>
2011-06-17 20:03:53 +0200

    actions stuff

--- fa/jquery/templates/admin/edit.pt.py
+++ fa/jquery/templates/admin/edit.pt.py
@@ -2,25 +2,16 @@ registry = dict(version=0)
 def bind():
     from cPickle import loads as _loads
     _lookup_attr = _loads('cchameleon.core.codegen\nlookup_attr\np1\n.')
-    _attrs_4362945488 = _loads('(dp1\nVclass\np2\nVui-icon ui-icon-circle-arrow-w\np3\ns.')
     _init_scope = _loads('cchameleon.core.utils\necontext\np1\n.')
-    _re_amp = _loads("cre\n_compile\np1\n(S'&(?!([A-Za-z]+|#[0-9]+);)'\np2\nI0\ntRp3\n.")
-    _attrs_4362944592 = _loads('(dp1\nVname\np2\nV_method\np3\nsVtype\np4\nVhidden\np5\nsVvalue\np6\nVPUT\np7\ns.')
-    _attrs_4362944656 = _loads('(dp1\nVclass\np2\nVfa_controls\np3\ns.')
-    _attrs_4362923984 = _loads('(dp1\n.')
-    _attrs_4362923856 = _loads('(dp1\nVaction\np2\nV\nsVmethod\np3\nVPOST\np4\nsVenctype\np5\nVmultipart/form-data\np6\ns.')
-    _attrs_4362944784 = _loads('(dp1\nVname\np2\nVnext\np3\nsVvalue\np4\nV\nsVtype\np5\nVhidden\np6\nsVid\np7\nVnext\np8\ns.')
-    _attrs_4362944976 = _loads('(dp1\nVclass\np2\nVui-icon ui-icon-check\np3\ns.')
+    _attrs_4367087248 = _loads('(dp1\nVclass\np2\nVfa_controls\np3\ns.')
+    _attrs_4367087120 = _loads('(dp1\nVname\np2\nVnext\np3\nsVvalue\np4\nV\nsVtype\np5\nVhidden\np6\nsVid\np7\nVnext\np8\ns.')
+    _attrs_4367086608 = _loads('(dp1\n.')
+    _attrs_4367086864 = _loads('(dp1\n.')
+    _attrs_4367086992 = _loads('(dp1\nVname\np2\nV_method\np3\nsVtype\np4\nVhidden\np5\nsVvalue\np6\nVPUT\np7\ns.')
     _init_stream = _loads('cchameleon.core.generation\ninitialize_stream\np1\n.')
-    _attrs_4362923728 = _loads('(dp1\n.')
     _init_default = _loads('cchameleon.core.generation\ninitialize_default\np1\n.')
-    _attrs_4362945104 = _loads('(dp1\nVtype\np2\nVsubmit\np3\ns.')
-    _attrs_4362945360 = _loads('(dp1\nVtype\np2\nVsubmit\np3\ns.')
-    _attrs_4362944912 = _loads("(dp1\nVhref\np2\nV#\nsVonclick\np3\nVvar f = jQuery(this).parents('form'); f.attr('action', window.location.href.replace('/edit', '/delete'));f.submit();\np4\nsVclass\np5\nVui-widget-header ui-widget-link ui-widget-button ui-state-error ui-corner-all\np6\ns.")
-    _attrs_4362944848 = _loads("(dp1\nVhref\np2\nV#\nsVonclick\np3\nVjQuery(this).parents('form').submit();\np4\nsVclass\np5\nVui-widget-header ui-widget-link ui-widget-button ui-corner-all\np6\ns.")
-    _attrs_4362945168 = _loads('(dp1\nVclass\np2\nVfa_controls ui-widget-header ui-widget-link ui-corner-all\np3\ns.')
+    _attrs_4367086736 = _loads('(dp1\nVaction\np2\nV\nsVmethod\np3\nVPOST\np4\nsVenctype\np5\nVmultipart/form-data\np6\ns.')
     _init_tal = _loads('cchameleon.core.generation\ninitialize_tal\np1\n.')
-    _attrs_4362945232 = _loads('(dp1\nVclass\np2\nVui-icon ui-icon-trash\np3\ns.')
     def render(econtext, rcontext=None):
         macros = econtext.get('macros')
         _translate = econtext.get('_translate')
@@ -41,15 +32,15 @@ def bind():
         def _callback_main(econtext, _repeat, _out=_out, _write=_write, _domain=_domain, **_ignored):
             if _repeat:
                 repeat.update(_repeat)
-            attrs = _attrs_4362923728
+            attrs = _attrs_4367086608
             _write(u'<div>\n      ')
-            attrs = _attrs_4362923856
+            attrs = _attrs_4367086736
             u"''"
             _write(u'<form action="" method="POST" enctype="multipart/form-data">\n        ')
             _default.value = default = ''
             u'fs.render()'
             _content = _lookup_attr(econtext['fs'], 'render')()
-            attrs = _attrs_4362923984
+            attrs = _attrs_4367086864
             u'_content'
             _write(u'<div>')
             _tmp1 = _content
@@ -68,21 +59,17 @@ def bind():
                     _tmp = str(_tmp)
                 _write(_tmp)
             _write(u'</div>\n        ')
-            attrs = _attrs_4362944592
+            attrs = _attrs_4367086992
             _write(u'<input type="hidden" name="_method" value="PUT" />\n        ')
-            attrs = _attrs_4362944656
-            _write(u'<div class="fa_controls">\n          ')
-            attrs = _attrs_4362944784
-            _write(u'<input type="hidden" id="next" name="next" value="" />\n          ')
-            attrs = _attrs_4362944848
-            _write(u'<a class="ui-widget-header ui-widget-link ui-widget-button ui-corner-all" href="#" onclick="jQuery(this).parents(\'form\').submit();">\n            ')
-            attrs = _attrs_4362944976
-            u"''"
-            _write(u'<span class="ui-icon ui-icon-check"></span>\n            ')
-            _default.value = default = ''
-            u'Save'
-            _content = u'Save'
+            attrs = _attrs_4367087120
+            u"u'\\n        '"
+            _write(u'<input type="hidden" id="next" name="next" value="" />\n        ')
+            _default.value = default = u'\n        '
+            u'actions.render(request)'
+            _content = _lookup_attr(econtext['actions'], 'render')(econtext['request'])
+            attrs = _attrs_4367087248
             u'_content'
+            _write(u'<div class="fa_controls">')
             _tmp1 = _content
             _tmp = _tmp1
             if (_tmp.__class__ not in (str, unicode, int, float, )):
@@ -97,112 +84,8 @@ def bind():
             if (_tmp is not None):
                 if not isinstance(_tmp, unicode):
                     _tmp = str(_tmp)
-                if ('&' in _tmp):
-                    if (';' in _tmp):
-                        _tmp = _re_amp.sub('&amp;', _tmp)
-                    else:
-                        _tmp = _tmp.replace('&', '&amp;')
-                if ('<' in _tmp):
-                    _tmp = _tmp.replace('<', '&lt;')
-                if ('>' in _tmp):
-                    _tmp = _tmp.replace('>', '&gt;')
                 _write(_tmp)
-            _write(u'\n            ')
-            attrs = _attrs_4362945104
-            _write(u'<input type="submit" />\n          </a>\n          ')
-            attrs = _attrs_4362944912
-            _write(u'<a class="ui-widget-header ui-widget-link ui-widget-button ui-state-error ui-corner-all" href="#" onclick="var f = jQuery(this).parents(\'form\'); f.attr(\'action\', window.location.href.replace(\'/edit\', \'/delete\'));f.submit();">\n            ')
-            attrs = _attrs_4362945232
-            u"''"
-            _write(u'<span class="ui-icon ui-icon-trash"></span>\n            ')
-            _default.value = default = ''
-            u'Delete'
-            _content = u'Delete'
-            u'_content'
-            _tmp1 = _content
-            _tmp = _tmp1
-            if (_tmp.__class__ not in (str, unicode, int, float, )):
-                try:
-                    _tmp = _tmp.__html__
-                except:
-                    _tmp = _translate(_tmp, domain=_domain, mapping=None, target_language=target_language, default=None)
-                else:
-                    _tmp = _tmp()
-                    _write(_tmp)
-                    _tmp = None
-            if (_tmp is not None):
-                if not isinstance(_tmp, unicode):
-                    _tmp = str(_tmp)
-                if ('&' in _tmp):
-                    if (';' in _tmp):
-                        _tmp = _re_amp.sub('&amp;', _tmp)
-                    else:
-                        _tmp = _tmp.replace('&', '&amp;')
-                if ('<' in _tmp):
-                    _tmp = _tmp.replace('<', '&lt;')
-                if ('>' in _tmp):
-                    _tmp = _tmp.replace('>', '&gt;')
-                _write(_tmp)
-            _write(u'\n            ')
-            attrs = _attrs_4362945360
-            _write(u'<input type="submit" />\n          </a>\n          ')
-            attrs = _attrs_4362945168
-            u'request.fa_url(request.model_name)'
-            _write(u'<a class="fa_controls ui-widget-header ui-widget-link ui-corner-all"')
-            _tmp1 = _lookup_attr(econtext['request'], 'fa_url')(_lookup_attr(econtext['request'], 'model_name'))
-            if (_tmp1 is _default):
-                _tmp1 = None
-            if ((_tmp1 is not None) and (_tmp1 is not False)):
-                if (_tmp1.__class__ not in (str, unicode, int, float, )):
-                    _tmp1 = unicode(_translate(_tmp1, domain=_domain, mapping=None, target_language=target_language, default=None))
-                else:
-                    if not isinstance(_tmp1, unicode):
-                        _tmp1 = str(_tmp1)
-                if ('&' in _tmp1):
-                    if (';' in _tmp1):
-                        _tmp1 = _re_amp.sub('&amp;', _tmp1)
-                    else:
-                        _tmp1 = _tmp1.replace('&', '&amp;')
-                if ('<' in _tmp1):
-                    _tmp1 = _tmp1.replace('<', '&lt;')
-                if ('>' in _tmp1):
-                    _tmp1 = _tmp1.replace('>', '&gt;')
-                if ('"' in _tmp1):
-                    _tmp1 = _tmp1.replace('"', '&quot;')
-                _write(((' href="' + _tmp1) + '"'))
-            _write(u'>\n            ')
-            attrs = _attrs_4362945488
-            u"''"
-            _write(u'<span class="ui-icon ui-icon-circle-arrow-w"></span>\n            ')
-            _default.value = default = ''
-            u'Cancel'
-            _content = u'Cancel'
-            u'_content'
-            _tmp1 = _content
-            _tmp = _tmp1
-            if (_tmp.__class__ not in (str, unicode, int, float, )):
-                try:
-                    _tmp = _tmp.__html__
-                except:
-                    _tmp = _translate(_tmp, domain=_domain, mapping=None, target_language=target_language, default=None)
-                else:
-                    _tmp = _tmp()
-                    _write(_tmp)
-                    _tmp = None
-            if (_tmp is not None):
-                if not isinstance(_tmp, unicode):
-                    _tmp = str(_tmp)
-                if ('&' in _tmp):
-                    if (';' in _tmp):
-                        _tmp = _re_amp.sub('&amp;', _tmp)
-                    else:
-                        _tmp = _tmp.replace('&', '&amp;')
-                if ('<' in _tmp):
-                    _tmp = _tmp.replace('<', '&lt;')
-                if ('>' in _tmp):
-                    _tmp = _tmp.replace('>', '&gt;')
-                _write(_tmp)
-            _write(u'\n          </a>\n        </div>\n      </form>\n    </div>\n')
+            _write(u'</div>\n      </form>\n    </div>\n')
         u"{'main': _callback_main}"
         _tmp = {'main': _callback_main, }
         u"main.macros['master']"
--- fa/jquery/templates/admin/show.pt.py
+++ fa/jquery/templates/admin/show.pt.py
@@ -1,18 +1,13 @@
 registry = dict(version=0)
 def bind():
     from cPickle import loads as _loads
-    _attrs_4363504080 = _loads('(dp1\nVclass\np2\nVui-icon ui-icon-check\np3\ns.')
     _lookup_attr = _loads('cchameleon.core.codegen\nlookup_attr\np1\n.')
     _init_scope = _loads('cchameleon.core.utils\necontext\np1\n.')
-    _re_amp = _loads("cre\n_compile\np1\n(S'&(?!([A-Za-z]+|#[0-9]+);)'\np2\nI0\ntRp3\n.")
-    _attrs_4363504464 = _loads('(dp1\nVclass\np2\nVui-icon ui-icon-circle-arrow-w\np3\ns.')
-    _attrs_4363504272 = _loads('(dp1\nVclass\np2\nVui-widget-header ui-widget-link ui-corner-all\np3\ns.')
-    _attrs_4363503696 = _loads('(dp1\n.')
-    _attrs_4363503888 = _loads('(dp1\n.')
+    _attrs_4367754384 = _loads('(dp1\n.')
+    _attrs_4367755536 = _loads('(dp1\nVclass\np2\nVfa_controls\np3\ns.')
     _init_stream = _loads('cchameleon.core.generation\ninitialize_stream\np1\n.')
-    _attrs_4362328912 = _loads('(dp1\nVclass\np2\nVfa_controls\np3\ns.')
     _init_default = _loads('cchameleon.core.generation\ninitialize_default\np1\n.')
-    _attrs_4362328720 = _loads('(dp1\nVclass\np2\nVui-widget-header ui-widget-link ui-widget-button ui-corner-all\np3\ns.')
+    _attrs_4367756240 = _loads('(dp1\n.')
     _init_tal = _loads('cchameleon.core.generation\ninitialize_tal\np1\n.')
     def render(econtext, rcontext=None):
         macros = econtext.get('macros')
@@ -34,13 +29,13 @@ def bind():
         def _callback_main(econtext, _repeat, _out=_out, _write=_write, _domain=_domain, **_ignored):
             if _repeat:
                 repeat.update(_repeat)
-            attrs = _attrs_4363503696
+            attrs = _attrs_4367754384
             u"''"
             _write(u'<div>\n      ')
             _default.value = default = ''
             u'fs.render()'
             _content = _lookup_attr(econtext['fs'], 'render')()
-            attrs = _attrs_4363503888
+            attrs = _attrs_4367756240
             u'_content'
             _write(u'<table>')
             _tmp1 = _content
@@ -58,41 +53,14 @@ def bind():
                 if not isinstance(_tmp, unicode):
                     _tmp = str(_tmp)
                 _write(_tmp)
+            u"u'\\n      '"
             _write(u'</table>\n      ')
-            attrs = _attrs_4362328912
-            _write(u'<div class="fa_controls">\n        ')
-            attrs = _attrs_4362328720
-            u"request.fa_url(request.model_name, request.model_id, 'edit')"
-            _write(u'<a class="ui-widget-header ui-widget-link ui-widget-button ui-corner-all"')
-            _tmp1 = _lookup_attr(econtext['request'], 'fa_url')(_lookup_attr(econtext['request'], 'model_name'), _lookup_attr(econtext['request'], 'model_id'), 'edit')
-            if (_tmp1 is _default):
-                _tmp1 = None
-            if ((_tmp1 is not None) and (_tmp1 is not False)):
-                if (_tmp1.__class__ not in (str, unicode, int, float, )):
-                    _tmp1 = unicode(_translate(_tmp1, domain=_domain, mapping=None, target_language=target_language, default=None))
-                else:
-                    if not isinstance(_tmp1, unicode):
-                        _tmp1 = str(_tmp1)
-                if ('&' in _tmp1):
-                    if (';' in _tmp1):
-                        _tmp1 = _re_amp.sub('&amp;', _tmp1)
-                    else:
-                        _tmp1 = _tmp1.replace('&', '&amp;')
-                if ('<' in _tmp1):
-                    _tmp1 = _tmp1.replace('<', '&lt;')
-                if ('>' in _tmp1):
-                    _tmp1 = _tmp1.replace('>', '&gt;')
-                if ('"' in _tmp1):
-                    _tmp1 = _tmp1.replace('"', '&quot;')
-                _write(((' href="' + _tmp1) + '"'))
-            _write(u'>\n          ')
-            attrs = _attrs_4363504080
-            u"''"
-            _write(u'<span class="ui-icon ui-icon-check"></span>\n          ')
-            _default.value = default = ''
-            u'Edit'
-            _content = u'Edit'
+            _default.value = default = u'\n      '
+            u'actions.render(request)'
+            _content = _lookup_attr(econtext['actions'], 'render')(econtext['request'])
+            attrs = _attrs_4367755536
             u'_content'
+            _write(u'<div class="fa_controls">')
             _tmp1 = _content
             _tmp = _tmp1
             if (_tmp.__class__ not in (str, unicode, int, float, )):
@@ -107,74 +75,8 @@ def bind():
             if (_tmp is not None):
                 if not isinstance(_tmp, unicode):
                     _tmp = str(_tmp)
-                if ('&' in _tmp):
-                    if (';' in _tmp):
-                        _tmp = _re_amp.sub('&amp;', _tmp)
-                    else:
-                        _tmp = _tmp.replace('&', '&amp;')
-                if ('<' in _tmp):
-                    _tmp = _tmp.replace('<', '&lt;')
-                if ('>' in _tmp):
-                    _tmp = _tmp.replace('>', '&gt;')
                 _write(_tmp)
-            _write(u'\n        </a>\n        ')
-            attrs = _attrs_4363504272
-            u'request.fa_url(request.model_name)'
-            _write(u'<a class="ui-widget-header ui-widget-link ui-corner-all"')
-            _tmp1 = _lookup_attr(econtext['request'], 'fa_url')(_lookup_attr(econtext['request'], 'model_name'))
-            if (_tmp1 is _default):
-                _tmp1 = None
-            if ((_tmp1 is not None) and (_tmp1 is not False)):
-                if (_tmp1.__class__ not in (str, unicode, int, float, )):
-                    _tmp1 = unicode(_translate(_tmp1, domain=_domain, mapping=None, target_language=target_language, default=None))
-                else:
-                    if not isinstance(_tmp1, unicode):
-                        _tmp1 = str(_tmp1)
-                if ('&' in _tmp1):
-                    if (';' in _tmp1):
-                        _tmp1 = _re_amp.sub('&amp;', _tmp1)
-                    else:
-                        _tmp1 = _tmp1.replace('&', '&amp;')
-                if ('<' in _tmp1):
-                    _tmp1 = _tmp1.replace('<', '&lt;')
-                if ('>' in _tmp1):
-                    _tmp1 = _tmp1.replace('>', '&gt;')
-                if ('"' in _tmp1):
-                    _tmp1 = _tmp1.replace('"', '&quot;')
-                _write(((' href="' + _tmp1) + '"'))
-            _write(u'>\n          ')
-            attrs = _attrs_4363504464
-            u"''"
-            _write(u'<span class="ui-icon ui-icon-circle-arrow-w"></span>\n          ')
-            _default.value = default = ''
-            u'Back'
-            _content = u'Back'
-            u'_content'
-            _tmp1 = _content
-            _tmp = _tmp1
-            if (_tmp.__class__ not in (str, unicode, int, float, )):
-                try:
-                    _tmp = _tmp.__html__
-                except:
-                    _tmp = _translate(_tmp, domain=_domain, mapping=None, target_language=target_language, default=None)
-                else:
-                    _tmp = _tmp()
-                    _write(_tmp)
-                    _tmp = None
-            if (_tmp is not None):
-                if not isinstance(_tmp, unicode):
-                    _tmp = str(_tmp)
-                if ('&' in _tmp):
-                    if (';' in _tmp):
-                        _tmp = _re_amp.sub('&amp;', _tmp)
-                    else:
-                        _tmp = _tmp.replace('&', '&amp;')
-                if ('<' in _tmp):
-                    _tmp = _tmp.replace('<', '&lt;')
-                if ('>' in _tmp):
-                    _tmp = _tmp.replace('>', '&gt;')
-                _write(_tmp)
-            _write(u'\n        </a>\n      </div>\n    </div>\n')
+            _write(u'</div>\n    </div>\n')
         u"{'main': _callback_main}"
         _tmp = {'main': _callback_main, }
         u"main.macros['master']"
